2c39f1a9013226651477e993948c42baf91969cc
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-08 20:22:49 +0200

    [back][auth] add tests to user & role entities

---
+++ BACKEND/services/auth-service/src/test/java/com/adelium/authservice/entity/UserTests.java
@@ -0,0 +1,18 @@
+package com.adelium.authservice.entity;
+
+import static org.assertj.core.api.Assertions.*;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+@DataJpaTest
+public class UserTests {
+
+    public void createUserTest(){
+        User user = new User();
+        user.setUsername("username");
+        user.setEmail("username@adelium.com");
+        user.setPassword("password");
+
+        assertThat(user.getUsername()).isEqualTo("username");
+        assertThat(user.getEmail()).isEqualTo("username@adelium.com");
+        assertThat(user.getPassword()).isEqualTo("password");
+    }
+}
