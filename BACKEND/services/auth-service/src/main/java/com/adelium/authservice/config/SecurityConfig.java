99495db7ca93d938c906bd8eb593c3cef0062be8
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-08 20:21:23 +0200

    [back][auth] add config.SecurityConfig.java

---
+++ BACKEND/services/auth-service/src/main/java/com/adelium/authservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.adelium.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/","/home").permitAll()
+                        .anyRequest().authenticated()
+                ).formLogin((form)-> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+}
