52a88b21f121df3f06cc11896667c66df38deca0
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-13 23:06:35 +0200

    [back][auth] add Auth Controller and service

---
+++ backend/services/auth-service/src/main/java/com/adelium/authservice/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.adelium.authservice.controller;
+
+import com.adelium.authservice.security.AuthenticationRequest;
+import com.adelium.authservice.security.AuthenticationResponse;
+import com.adelium.authservice.security.RegisterRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.adelium.authservice.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService service;
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response) throws IOException {
+        service.refreshToken(request, response);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("hello");
+    }
+}
