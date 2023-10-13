c73ce604962c9dd23f1ea4442819b85b0b1ebc07
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-13 04:23:31 +0200

    [front] delete unused files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  template: ` <main class="flex items-center justify-center w-screen h-screen">
+      <div
+        class="text-2xl  text-blue-900 bg-blue-200 border-blue-800 border shadow-md rounded-xl p-4 m-6">
+        test tailwindcss
+      </div>
+    </main>
+
+    <router-outlet></router-outlet>`,
 })
 export class AppComponent {
-  title = 'adelium';
+  title = 'adelium'
 }
