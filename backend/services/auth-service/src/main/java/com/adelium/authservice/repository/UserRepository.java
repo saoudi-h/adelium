c858834159895b995f74f071087e85b5675fe68a
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-13 22:33:12 +0200

    [back][auth] update UserRepository

--- backend/services/auth-service/src/main/java/com/adelium/authservice/repository/UserRepository.java
+++ backend/services/auth-service/src/main/java/com/adelium/authservice/repository/UserRepository.java
@@ -6,7 +6,11 @@ import com.adelium.common.repository.BaseRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @Transactional
 public interface UserRepository extends BaseRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
