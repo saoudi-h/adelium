1be56d57777217353329d5a4e0fc0830b762039d
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-13 22:58:38 +0200

    [back][auth] update SecurityConfig

--- backend/services/auth-service/src/main/java/com/adelium/authservice/config/SecurityConfig.java
+++ backend/services/auth-service/src/main/java/com/adelium/authservice/config/SecurityConfig.java
@@ -2,28 +2,35 @@ package com.adelium.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/","/home").permitAll()
-                        .anyRequest().authenticated()
-                ).formLogin((form)-> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll);
-        return http.build();
-    }
+        private final JwtAuthFilter jwtAuthFilter;
+
+        private final AuthenticationProvider authenticationProvider;
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(AbstractHttpConfigurer::disable)
+                                .authorizeHttpRequests((requests) -> requests
+                                                // whitelist no permission needed
+                                                .requestMatchers("/auth/**").permitAll()
+                                                // not in white list routes should be authenticated
+                                                .anyRequest().authenticated())
+                                .authenticationProvider(authenticationProvider)
+                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                return http.build();
+        }
 }
