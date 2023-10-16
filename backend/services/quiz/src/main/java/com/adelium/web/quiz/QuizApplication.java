470d3a0b49ee27c6c9a7073aa07deb5dc3a8e934
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-16 15:55:37 +0200

    [back] create quiz-service

---
+++ backend/services/quiz/src/main/java/com/adelium/web/quiz/QuizApplication.java
@@ -0,0 +1,13 @@
+/* (C)2023 */
+package com.adelium.web.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizApplication.class, args);
+    }
+}
---
+++ backend/services/quiz/src/test/java/com/adelium/web/quiz/QuizApplicationTests.java
@@ -0,0 +1,12 @@
+/* (C)2023 */
+package com.adelium.web.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
