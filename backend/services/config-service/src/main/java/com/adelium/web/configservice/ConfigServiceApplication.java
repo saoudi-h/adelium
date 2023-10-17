853f3bef1be0bd97139b385de93506ff27b3bbba
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-17 05:27:20 +0200

    [back] add config-service

---
+++ backend/services/config-service/src/main/java/com/adelium/web/configservice/ConfigServiceApplication.java
@@ -0,0 +1,15 @@
+/* (C)2023 */
+package com.adelium.web.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
---
+++ backend/services/config-service/src/test/java/com/adelium/web/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,12 @@
+/* (C)2023 */
+package com.adelium.web.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
