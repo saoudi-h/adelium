091896493a213e0d9a84a661ceb97177cff9cd4c
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-18 13:15:26 +0200

    [back][config] fix wrong annotation

--- backend/services/config-service/src/main/java/com/adelium/web/configservice/ConfigServiceApplication.java
+++ backend/services/config-service/src/main/java/com/adelium/web/configservice/ConfigServiceApplication.java
@@ -3,10 +3,10 @@ package com.adelium.web.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
