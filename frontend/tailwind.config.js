ee88e02cf1cb339f71ffd377e098d39e4b79c91b
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-19 01:06:51 +0200

    [front] install daisyui + config theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,ts}',
-    "./node_modules/flowbite/**/*.js"
-],
+  content: ['./src/**/*.{html,ts}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require('flowbite/plugin'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#a991f7',
+          secondary: '#f6d860',
+          accent: '#37cdbe',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+      'business',
+    ],
+  },
 }
