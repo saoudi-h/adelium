9c5ef38f1af7a989f0719021e2e2d0e94d69dd9b
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-21 17:10:10 +0200

    [front] update navbar

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component'
+import { Component } from '@angular/core'
+import { RouterLink, RouterLinkActive } from '@angular/router'
 import { MegaMenuComponent } from './mega-menu.component'
+import { SearchComponent } from './search/search.component'
+import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component'
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
-  imports: [CommonModule, ThemeSwitcherComponent, MegaMenuComponent],
+  imports: [
+    CommonModule,
+    ThemeSwitcherComponent,
+    MegaMenuComponent,
+    RouterLink,
+    RouterLinkActive,
+    SearchComponent,
+  ],
 })
 export class NavbarComponent {}
