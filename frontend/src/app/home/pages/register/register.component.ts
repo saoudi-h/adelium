5bc415acbb415b083cd0b30e6ef6b07d2c0061ae
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-21 17:08:10 +0200

    [front] up register page

--- frontend/src/app/home/pages/register/register.component.ts
+++ frontend/src/app/home/pages/register/register.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { SvgRegisterComponent } from './svg-register.component'
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SvgRegisterComponent],
   templateUrl: './register.component.html',
   styles: [],
 })
