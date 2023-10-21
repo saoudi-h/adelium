31466b7f88e70e6ff46d233102096fd180af0419
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-21 17:04:22 +0200

    [front] up home-layout

--- frontend/src/app/home/home-layout.component.ts
+++ frontend/src/app/home/home-layout.component.ts
@@ -1,17 +1,17 @@
-import { FooterComponent } from './../shared/components/footer/footer.component'
-import { NavbarComponent } from '../shared/components/navbar/navbar.component'
-import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { NavbarComponent } from '../shared/components/navbar/navbar.component'
+import { FooterComponent } from './../shared/components/footer/footer.component'
 import { HomeRoutingModule } from './home-routing.module'
 @Component({
   selector: 'app-home-layout',
   standalone: true,
   imports: [CommonModule, NavbarComponent, FooterComponent, HomeRoutingModule],
   template: `
-    <div class="flex flex-col min-h-screen justify-between">
+    <div class="relative flex min-h-screen flex-col justify-between overflow-hidden">
       <app-navbar></app-navbar>
       <main
-        class="bg-base-200 w-full h-max flex-1 mt-20 flex items-center justify-center content-center">
+        class="relative mt-20 flex h-max w-full flex-1 content-center items-center justify-center overflow-hidden bg-gradient-to-t from-base-100 to-base-300">
         <router-outlet></router-outlet>
       </main>
       <app-footer></app-footer>
