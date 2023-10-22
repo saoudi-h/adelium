2d67efdd738ac1de4790e377e42f499ea7ccb346
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-22 20:57:23 +0200

    [front] update home page + landing page

--- frontend/src/app/home/pages/home/home.component.ts
+++ frontend/src/app/home/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { FooterComponent } from '../../../shared/components/footer/footer.component'
-import { LandingComponent } from '../../../shared/components/landing/landing.component'
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component'
+import { LandingComponent } from './landing/landing.component'
 
 @Component({
   selector: 'app-home',
-  template: '<app-landing></app-landing>',
+  template: '<app-landing class="grow"></app-landing>',
+  styleUrls: ['./home.component.sass'],
   standalone: true,
   imports: [NavbarComponent, LandingComponent, FooterComponent],
 })
