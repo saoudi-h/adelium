4f252fde28d6391e42645e6a756c5660970a0897
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-22 20:58:03 +0200

    [front] update pages

--- frontend/src/app/home/pages/not-found/not-found.component.ts
+++ frontend/src/app/home/pages/not-found/not-found.component.ts
@@ -5,23 +5,21 @@ import { SvgNotFoundComponent } from './svg-not-found.component'
 @Component({
   selector: 'app-not-found',
   template: ` <section class="container mx-auto flex h-full">
-    <div class="hero mb-12 mt-20 sm:mt-12">
+    <div class="hero mb-12 mt-8 sm:mt-20">
       <div class="hero-content flex-col lg:flex-row-reverse">
         <div
           class="flex w-full flex-col items-center justify-center text-center lg:w-1/2 lg:px-2 xl:px-0">
-          <h1 class="font-serif">
-            <p class="text-6xl font-bold tracking-wider md:text-7xl lg:text-8xl">404</p>
-            <p class="mt-2 text-3xl font-bold tracking-wider md:text-4xl lg:text-5xl">
-              Page introuvable.
-            </p>
+          <h1 class="font-serif text-res-big">
+            <p class="text-6xl font-bold tracking-wider text-accent md:text-7xl lg:text-8xl">404</p>
+            <p class="font-serif text-res-h1 font-bold">Page introuvable.</p>
           </h1>
-          <p class="my-12 text-lg text-gray-500 md:text-xl lg:text-2xl">
+          <p class="py-4 font-light text-base-content/60 md:text-lg xl:text-2xl">
             Sorry, the page you are looking for could not be found.
           </p>
-          <a class="btn btn-primary normal-case">
+          <a class="btn btn-primary btn-lg font-normal">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
+              class="mr-2 h-6 w-6"
               viewBox="0 0 20 20"
               fill="currentColor">
               <path
