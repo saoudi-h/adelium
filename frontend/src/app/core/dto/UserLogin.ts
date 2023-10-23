d9059399118894ed950c06d35f7a3ab8194ae0c4
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-23 23:59:45 +0200

    [front] add UserLogin dto

---
+++ frontend/src/app/core/dto/UserLogin.ts
@@ -0,0 +1,14 @@
+/**
+ * Represents user login information.
+ */
+export interface UserLogin {
+  /**
+   * The user's username (email address).
+   */
+  username: string
+
+  /**
+   * The user's password for authentication.
+   */
+  password: string
+}
