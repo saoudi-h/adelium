53ee61d27e37a4f0b8ec0f07e7ec108c311083bc
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-25 23:05:49 +0200

    [back] add UserService

---
+++ backend/services/auth-service/src/main/java/com/adelium/web/authservice/service/UserService.java
@@ -0,0 +1,27 @@
+/* (C)2023 */
+package com.adelium.web.authservice.service;
+
+import com.adelium.web.authservice.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UserService implements UserDetailsService {
+
+    @Autowired private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByUsername(username)
+                .orElseThrow(
+                        () ->
+                                new UsernameNotFoundException(
+                                        "User with username: " + username + " not found."));
+    }
+}
