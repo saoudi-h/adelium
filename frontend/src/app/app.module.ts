75edad44e1cf9acaf11a9e26a8f0e72a02a6fa31
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-25 20:31:03 +0200

    [front] update app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { ThemeService } from '@/core/services/theme.service'
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { HomeModule } from '@home/home.module'
 import { AdminModule } from './admin/admin.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HomeLayoutComponent } from './home/home-layout.component'
 
 @NgModule({
     declarations: [AppComponent],
@@ -14,7 +14,7 @@ import { HomeLayoutComponent } from './home/home-layout.component'
         HttpClientModule,
         AppRoutingModule,
         AdminModule,
-        HomeLayoutComponent,
+        HomeModule,
     ],
     providers: [ThemeService],
     bootstrap: [AppComponent],
