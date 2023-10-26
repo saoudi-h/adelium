466b4a69ac84dcf271a45ff48b8685f9c549c4d8
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-26 23:10:07 +0200

    [front] turn front to pwa app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { ThemeService } from '@/core/services/theme.service'
 import { HttpClientModule } from '@angular/common/http'
-import { NgModule } from '@angular/core'
+import { NgModule, isDevMode } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { ServiceWorkerModule } from '@angular/service-worker'
 import { HomeModule } from '@home/home.module'
 import { AdminModule } from './admin/admin.module'
 import { AppRoutingModule } from './app-routing.module'
@@ -15,6 +16,12 @@ import { AppComponent } from './app.component'
         AppRoutingModule,
         AdminModule,
         HomeModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
     ],
     providers: [ThemeService],
     bootstrap: [AppComponent],
