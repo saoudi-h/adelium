0103726e98fc5d36824177a10475b031adbdfb36
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-28 22:23:15 +0200

    [back][auth] set sessionManagement and logout

--- backend/services/auth-service/src/main/java/com/adelium/web/authservice/config/SecurityConfig.java
+++ backend/services/auth-service/src/main/java/com/adelium/web/authservice/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 /* (C)2023 */
 package com.adelium.web.authservice.config;
 
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +10,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +23,8 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
     private final AuthenticationProvider authenticationProvider;
+
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,8 +38,16 @@ public class SecurityConfig {
                                         // not in white list routes should be authenticated
                                         .anyRequest()
                                         .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(
+                        logout ->
+                                logout.logoutUrl("/auth/logout")
+                                        .addLogoutHandler(logoutHandler)
+                                        .logoutSuccessHandler(
+                                                (request, response, authentication) ->
+                                                        SecurityContextHolder.clearContext()));
         return http.build();
     }
 }
