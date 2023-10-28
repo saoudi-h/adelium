80313336d588aa017d36a9721fef93b12bfea459
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-28 05:02:15 +0200

    [front] add auth and not-auth guards

---
+++ frontend/src/app/auth/guards/not-auth.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core'
+import { CanActivateFn, Router } from '@angular/router'
+import { AuthService } from '@core/services/auth.service'
+
+export const notAuthGuard: CanActivateFn = (route, state) => {
+    const authService = inject(AuthService)
+    const router = inject(Router)
+
+    if (!authService.isLoggedIn()) {
+        return true
+    }
+
+    return router.parseUrl('/')
+}
