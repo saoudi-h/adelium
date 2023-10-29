598dcc0579cc11f8604baad29d0d67630cad2a39
Hakim Saoudi <saoudihakim@gmail.com>
2023-10-29 02:23:45 +0100

    [back][discovery] rename discovery-server to discovery-service

---
+++ backend/services/discovery-service/src/test/java/com/adelium/web/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,12 @@
+/* (C)2023 */
+package com.adelium.web.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
