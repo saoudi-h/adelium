7ff02313fa062586026b0a77887e42e1d20e8b55
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-10 02:20:43 +0100

    [back][gateway] cleanup

--- backend/services/gateway-service/src/main/java/com/adelium/web/gatewayservice/GatewayServiceApplication.java
+++ backend/services/gateway-service/src/main/java/com/adelium/web/gatewayservice/GatewayServiceApplication.java
@@ -23,26 +23,6 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    //    @Bean
-    //    @Lazy(false)
-    //    public List<GroupedOpenApi> apis(RouteDefinitionLocator locator) {
-    //        List<GroupedOpenApi> groups = new ArrayList<>();
-    //        List<RouteDefinition> definitions =
-    // locator.getRouteDefinitions().collectList().block();
-    //        for (RouteDefinition definition : definitions) {
-    //            System.out.println("id: " + definition.getId() + "  " +
-    // definition.getUri().toString());
-    //        }
-    //        definitions.stream().filter(routeDefinition ->
-    // routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-    //            String name = routeDefinition.getId().replaceAll("-service", "");
-    //            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
-    //        });
-    //        return groups;
-    //    }
-
-    //
-    // https://medium.com/@pubuduc.14/swagger-openapi-specification-3-integration-with-spring-cloud-gateway-part-2-1d670d4ab69a
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
