b07c12f588670e4cd0cbe898ceecfe0a72897b54
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-18 23:27:54 +0100

    [back][quiz] up question package

--- backend/services/quiz-service/src/main/java/com/adelium/web/quizservice/entity/question/QuestionMCQ.java
+++ backend/services/quiz-service/src/main/java/com/adelium/web/quizservice/entity/question/QuestionMCQ.java
@@ -6,25 +6,37 @@ import com.adelium.web.quizservice.core.media.BaseMedia;
 import com.adelium.web.quizservice.core.media.MediaText;
 import com.adelium.web.quizservice.core.question.BaseQuestion;
 import com.adelium.web.quizservice.core.question.Optionable;
-import com.adelium.web.quizservice.entity.option.OptionTrueFalse;
+import com.adelium.web.quizservice.entity.option.OptionMCQ;
 import jakarta.persistence.*;
 import java.util.Set;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 /**
- * Represents a true/false quiz question.
+ * Represents a multiple choice quiz question.
+ * <p>
+ *     Extends the AbstractQuestion class and implements the Propositional interface.
+ *     It is a generic class, with the type of the options as a parameter.
+ *     It is also a JPA entity.
+ * </p>
  *
- * Extends the AbstractQuestion class and implements the Propositional interface.
+ * @see BaseQuestion
+ * @see Optionable
+ * @see OptionMCQ
+ * @see BaseMedia
+ * @see MediaText
  */
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @SuperBuilder
-@Entity(name = "question_true_false")
-public class QuestionTrueFalse extends BaseQuestion<BaseMedia>
-        implements Optionable<OptionTrueFalse, BaseMedia, MediaText> {
+@Entity(name = "question_mcq")
+public class QuestionMCQ extends BaseQuestion<BaseMedia>
+        implements Optionable<OptionMCQ, BaseMedia, MediaText> {
 
     /**
      * The number of options of the question.
@@ -38,10 +50,10 @@ public class QuestionTrueFalse extends BaseQuestion<BaseMedia>
     @JoinColumn(name = "media_id")
     private BaseMedia content;
 
-    private static final String type = "true_false";
+    private static final String type = "mcq";
 
     @OneToMany(mappedBy = "question")
-    private Set<OptionTrueFalse> options;
+    private Set<OptionMCQ> options;
 
     /**
      * Is the question enabled?
