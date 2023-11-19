40b2cc1170979f1b8bae9cde1d93215821e525ac
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-19 23:57:00 +0100

    [back][common] move jwtAuthFilter + JwtService from auth to common

---
+++ backend/common/src/main/java/com/adelium/web/common/security/JwtAuthFilter.java
@@ -0,0 +1,87 @@
+/* (C)2023 */
+package com.adelium.web.common.security;
+
+import com.adelium.web.common.client.AuthServiceClient;
+import com.adelium.web.common.dto.UserDetailsDTO;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * JwtAuthFilter is a custom security filter that extends OncePerRequestFilter, ensuring
+ * it's applied once per request within a single request thread. It's responsible for
+ * intercepting HTTP requests and handling JWT-based authentication.
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    private final AuthServiceClient authServiceClient;
+
+    /**
+     * Custom filter logic for JWT authentication.
+     *
+     * @param request     The incoming HttpServletRequest.
+     * @param response    The outgoing HttpServletResponse.
+     * @param filterChain The filter chain provided by the Spring security filter chain.
+     * @throws ServletException if a servlet-specific error occurs.
+     * @throws IOException      if an I/O error occurs during request processing.
+     */
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String username;
+
+        System.out.println("authHeader : " + authHeader);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // TODO: try final String jwtToken = authHeader.split(" ")[1].trim();
+        jwtToken = authHeader.substring(7);
+        username = jwtService.extractUsername(jwtToken);
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // user not connected
+            UserDetailsDTO userDetails = getUserDetails(username);
+
+            if (userDetails != null && jwtService.isTokenValid(jwtToken, userDetails)) {
+                Claims claims = jwtService.extractAllClaims(jwtToken);
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails, claims, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * Retrieves user details from the authentication service based on the username.
+     *
+     * @param username The username to retrieve user details for.
+     * @return UserDetailsDTO object containing user details.
+     */
+    protected UserDetailsDTO getUserDetails(String username) {
+        return authServiceClient.getUser(username).getBody();
+    }
+}
---
+++ backend/common/src/main/java/com/adelium/web/common/security/JwtService.java
@@ -0,0 +1,73 @@
+/* (C)2023 */
+package com.adelium.web.common.security;
+
+import com.adelium.web.common.dto.TokenDTO;
+import com.adelium.web.common.dto.UserDetailsDTO;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import java.util.function.Function;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, UserDetailsDTO userDetails) {
+        final TokenDTO serverToken =
+                userDetails.getTokens().stream()
+                        .filter(t -> t.equals(token))
+                        .findFirst()
+                        .orElse(null);
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()))
+                && !isTokenExpired(token)
+                && serverToken != null
+                && !serverToken.isRevoked();
+    }
+
+    private boolean isTokenExpired(String token) {
+        try {
+            return extractExpiration(token).before(new Date());
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Claims extractAllClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(getSignInKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
