9430a09342490f9895f6219e9384b76ef75ef437
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-22 15:52:35 +0100

    [front] clean up

--- frontend/src/app/home/pages/home/home.component.ts
+++ frontend/src/app/home/pages/home/home.component.ts
@@ -1,23 +1,13 @@
-import { Component, inject } from '@angular/core'
-import { AuthService } from '@auth/services/auth.service'
+import { Component } from '@angular/core'
 import { FooterComponent } from '../../../shared/components/footer/footer.component'
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component'
 import { LandingComponent } from './landing/landing.component'
 
 @Component({
     selector: 'app-home',
-    template: `
-        <app-landing class="grow"></app-landing>
-        <button class="btn btn-circle" (click)="test()">T</button>
-    `,
+    template: ` <app-landing class="grow"></app-landing>`,
     styleUrls: ['./home.component.sass'],
     standalone: true,
     imports: [NavbarComponent, LandingComponent, FooterComponent],
 })
-export class HomeComponent {
-    authService = inject(AuthService)
-    test() {
-        const res = this.authService.hasTokenExpired()
-        console.log('is token expired : ', res)
-    }
-}
+export class HomeComponent {}
