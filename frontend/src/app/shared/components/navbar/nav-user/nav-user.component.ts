964aec5833411cbddd1308df289d1aefc4635e63
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-22 15:52:20 +0100

    [front] update nav-user with user data + add capitalize pipe

--- frontend/src/app/shared/components/navbar/nav-user/nav-user.component.ts
+++ frontend/src/app/shared/components/navbar/nav-user/nav-user.component.ts
@@ -2,22 +2,24 @@ import { CommonModule } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 import { AuthService } from '@auth/services/auth.service'
+import { UserToken } from '@core/dto/UserToken'
+import { CapitalizePipe } from '@shared/pipe/capitalize.pipe'
 
 @Component({
     selector: 'app-nav-user',
     standalone: true,
-    imports: [CommonModule, RouterLink, RouterLinkActive],
+    imports: [CommonModule, RouterLink, RouterLinkActive, CapitalizePipe],
     templateUrl: './nav-user.component.html',
     styles: [],
 })
 export class NavUserComponent implements OnInit {
-    public isLoggedIn = false
+    public user: UserToken | null = null
 
     constructor(private authService: AuthService) {}
 
     ngOnInit() {
-        this.authService.isLoggedIn$.subscribe(isLoggedIn => {
-            this.isLoggedIn = isLoggedIn
+        this.authService.user$.subscribe(user => {
+            this.user = user
         })
     }
 
