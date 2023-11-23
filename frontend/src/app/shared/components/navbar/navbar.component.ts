0f6b87df2b6bc702f9fe4523611b31aaea95b30c
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-23 20:02:38 +0100

    [front] move widget components to widgets

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -2,21 +2,17 @@ import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 import { MegaMenuComponent } from './mega-menu.component'
-import { NavUserComponent } from './nav-user/nav-user.component'
-import { SearchComponent } from './search/search.component'
-import { ThemeSwitcherComponent } from './theme-switcher/theme-switcher.component'
+import { WidgetsComponent } from './widgets/widgets.component'
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
     imports: [
         CommonModule,
-        ThemeSwitcherComponent,
         MegaMenuComponent,
         RouterLink,
         RouterLinkActive,
-        SearchComponent,
-        NavUserComponent,
+        WidgetsComponent,
     ],
 })
 export class NavbarComponent {}
