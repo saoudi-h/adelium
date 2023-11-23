f77de6f8e1938920596433efd1a47f1d8e1220e5
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-23 19:28:33 +0100

    [front] install ngx-toastr

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { ThemeService } from '@/core/services/theme.service'
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule, isDevMode } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ServiceWorkerModule } from '@angular/service-worker'
+import { NotificationService } from '@core/services/notification.service'
 import { HomeModule } from '@home/home.module'
+import { ToastrModule } from 'ngx-toastr'
 import { AdminModule } from './admin/admin.module'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -16,6 +19,8 @@ import { AppComponent } from './app.component'
         AppRoutingModule,
         AdminModule,
         HomeModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: !isDevMode(),
             // Register the ServiceWorker as soon as the application is stable
@@ -23,7 +28,7 @@ import { AppComponent } from './app.component'
             registrationStrategy: 'registerWhenStable:30000',
         }),
     ],
-    providers: [ThemeService],
+    providers: [ThemeService, NotificationService],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
