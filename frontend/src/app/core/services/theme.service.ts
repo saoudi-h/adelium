b32c295353788d8276341cef419c206533c898ee
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-26 18:31:48 +0100

    [front] up theme.service

--- frontend/src/app/core/services/theme.service.ts
+++ frontend/src/app/core/services/theme.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs'
 
+/**
+ * Theme type
+ */
 export enum ThemeType {
     Dark = 'dark',
     Light = 'light',
@@ -7,24 +11,39 @@ export enum ThemeType {
 }
 
 /**
- * Service gérant le thème de l'application.
+ * Service for managing the theme of the application.
+ * @example
+ * // Set the theme to dark
+ * this.themeService.setTheme(ThemeType.Dark)
+ * @example
+ * // Set the theme to light
+ * this.themeService.setTheme(ThemeType.Light)
  */
 @Injectable({
     providedIn: 'root',
 })
 export class ThemeService {
+    /**
+     * The key used to store the theme in the local storage.
+     */
     private readonly THEME_KEY = 'color-theme'
+    /**
+     * The theme subject.
+     */
+    private themeSubject = new BehaviorSubject<ThemeType>(this.getTheme())
 
     /**
-     * Initialise le thème en fonction des préférences de l'utilisateur.
+     * Init the theme of the application.
+     *
+     * This method should be called in the constructor of the AppComponent.
      */
     initTheme() {
         this.setTheme(this.getTheme())
     }
 
     /**
-     * Définit le thème de l'application.
-     * @param theme Le type de thème à définir.
+     * Defines the theme of the application.
+     * @param theme The theme to set.
      */
     setTheme(theme: ThemeType): void {
         // Résout le thème en fonction des préférences de l'utilisateur.
@@ -33,11 +52,13 @@ export class ThemeService {
         document.documentElement.setAttribute('data-theme', resolvedTheme)
 
         localStorage.setItem(this.THEME_KEY, theme)
+        this.themeSubject.next(theme)
     }
 
     /**
-     * Obtient le thème actuel de l'application.
-     * @returns Le type de thème actuel.
+     * Gets the current theme of the application.
+     *
+     * @returns The current theme of the application.
      */
     getTheme(): ThemeType {
         const storedTheme = localStorage.getItem(this.THEME_KEY)
@@ -50,8 +71,16 @@ export class ThemeService {
     }
 
     /**
-     * Obtient la préférence de thème du système de l'utilisateur.
-     * @returns Le type de thème préféré par le système.
+     * Gets the theme changes observable.
+     * @returns The theme changes observable.
+     */
+    get themeChanges() {
+        return this.themeSubject.asObservable()
+    }
+
+    /**
+     * Gets the system theme preference.
+     * @returns The system theme preference.
      */
     private getSystemThemePreference(): ThemeType {
         return window.matchMedia('(prefers-color-scheme: dark)').matches
