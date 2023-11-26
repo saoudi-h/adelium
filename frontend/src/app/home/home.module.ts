2b796d9e1da0f4c4345aa239270df6e58f2bbdd2
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-26 18:32:48 +0100

    [front] cleanup

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,20 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { AuthModule } from '@auth/auth.module'
-import { MegaMenuComponent } from '@shared/components/navbar/mega-menu.component'
-import { NavbarComponent } from '../shared/components/navbar/navbar.component'
-import { FooterComponent } from './../shared/components/footer/footer.component'
 import { HomeRoutingModule } from './home-routing.module'
 
 @NgModule({
     declarations: [],
-    imports: [
-        CommonModule,
-        HomeRoutingModule,
-        FooterComponent,
-        NavbarComponent,
-        MegaMenuComponent,
-        AuthModule,
-    ],
+    imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
