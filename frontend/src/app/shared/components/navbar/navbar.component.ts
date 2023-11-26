ed9fc2fb24b645b4fed506cdc018249959b6664a
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-26 18:32:24 +0100

    [front] up navbar

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,63 @@
 import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
 import { LogoComponent } from '../logo/logo.component'
 import { MegaMenuComponent } from './mega-menu.component'
 import { WidgetsComponent } from './widgets/widgets.component'
+
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    templateUrl: './navbar.component.html',
+    template: `<header
+        class="sticky top-0 z-30 flex h-16 w-full justify-center bg-base-300 text-base-content backdrop-blur transition-all duration-100 [transform:translate3d(0,0,0)]">
+        <div [ngClass]="{ container: !headerWide }" class="navbar mx-auto">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label
+                        for="my-drawer-3"
+                        aria-label="open sidebar"
+                        class="btn btn-square btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </label>
+                </div>
+                <a
+                    class="btn btn-ghost text-xl normal-case"
+                    routerLink="/"
+                    routerLinkActive="router-link-active"
+                    ><app-logo
+                /></a>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li tabindex="0">
+                        <details>
+                            <summary>Parent</summary>
+                            <ul
+                                class="menu dropdown-content menu-sm left-[-50%] z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow xl:menu-horizontal lg:min-w-max"
+                                tabindex="0"
+                                megaMenu></ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <app-widgets />
+            </div>
+        </div>
+    </header>`,
     imports: [
         CommonModule,
         MegaMenuComponent,
@@ -17,4 +67,6 @@ import { WidgetsComponent } from './widgets/widgets.component'
         LogoComponent,
     ],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    @Input() headerWide = false
+}
