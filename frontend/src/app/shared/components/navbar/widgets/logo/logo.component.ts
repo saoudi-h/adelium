23bd6f455ce95949786b9981f6a50ea1ebb22de6
Hakim Saoudi <saoudihakim@gmail.com>
2023-11-27 01:50:09 +0100

    [front] fix missing import dependency

--- frontend/src/app/shared/components/navbar/widgets/logo/logo.component.ts
+++ frontend/src/app/shared/components/navbar/widgets/logo/logo.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 @Component({
     standalone: true,
     selector: '[logo-widget]',
+    imports: [RouterLink],
     template: ` <a routerLink="/" class="flex items-center text-lg font-black"
         >Adelium</a
     >`,
