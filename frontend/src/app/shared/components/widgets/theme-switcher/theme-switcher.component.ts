a26fab992e1931ea4837ddfe16aa114795813d90
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-02 23:54:45 +0100

    [front] update theme-switcher with ngrx

--- frontend/src/app/shared/components/widgets/theme-switcher/theme-switcher.component.ts
+++ frontend/src/app/shared/components/widgets/theme-switcher/theme-switcher.component.ts
@@ -1,11 +1,15 @@
-import { ThemeService, ThemeType } from '@/core/services/theme.service'
 import { CommonModule } from '@angular/common'
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { AppState } from '@reducers'
 import { SvgMoonComponent } from '@shared/components/icons/moon.component'
 import { SvgSunComponent } from '@shared/components/icons/sun.component'
 import { SvgSystemComponent } from '@shared/components/icons/system.component'
 import { DropdownComponent } from '@shared/components/utility/dropdown/dropdown.component'
-import { Subscription } from 'rxjs'
+import { setUserThemeChoice } from '@store/theme/theme.actions'
+import { selectUserThemeChoice } from '@store/theme/theme.selectors'
+import { ThemeChoice } from '@store/theme/theme.types'
+import { Observable } from 'rxjs'
 
 @Component({
     selector: '[theme-switcher-widget]',
@@ -19,42 +23,35 @@ import { Subscription } from 'rxjs'
     ],
     templateUrl: './theme-switcher.component.html',
 })
-export class ThemeSwitcherWidgetComponent implements OnInit, OnDestroy {
-    private themeSubscription!: Subscription
+export class ThemeSwitcherWidgetComponent implements OnInit {
+    selectedCurrentTheme$!: Observable<ThemeChoice>
 
     @ViewChild(DropdownComponent) dropdown!: DropdownComponent
-    selectedTheme: ThemeType = ThemeType.System
 
-    themes: { theme: ThemeType; label: string; component: unknown }[] = [
-        { theme: ThemeType.Light, label: 'Light', component: SvgSunComponent },
-        { theme: ThemeType.Dark, label: 'Dark', component: SvgMoonComponent },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    themes: { theme: ThemeChoice; label: string; component: any }[] = [
         {
-            theme: ThemeType.System,
+            theme: ThemeChoice.Light,
+            label: 'Light',
+            component: SvgSunComponent,
+        },
+        { theme: ThemeChoice.Dark, label: 'Dark', component: SvgMoonComponent },
+        {
+            theme: ThemeChoice.System,
             label: 'System',
             component: SvgSystemComponent,
         },
     ]
 
-    constructor(private themeService: ThemeService) {}
+    constructor(private store: Store<AppState>) {}
 
     ngOnInit(): void {
-        this.selectedTheme = this.themeService.getTheme()
-        this.themeSubscription = this.themeService.themeChanges.subscribe(
-            theme => {
-                this.selectedTheme = theme
-            }
-        )
+        // eslint-disable-next-line @ngrx/prefer-selector-in-select
+        this.selectedCurrentTheme$ = this.store.select(selectUserThemeChoice)
     }
 
-    setTheme(event: Event, theme: ThemeType): void {
+    setTheme(event: Event, theme: ThemeChoice): void {
         event.stopPropagation()
-        this.selectedTheme = theme
-        this.themeService.setThemeWithoutRoute(theme)
-    }
-
-    ngOnDestroy(): void {
-        if (this.themeSubscription) {
-            this.themeSubscription.unsubscribe()
-        }
+        this.store.dispatch(setUserThemeChoice({ choice: theme }))
     }
 }
