7321819f3fa28c274576d138d370f0125a46bc85
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-03 08:08:28 +0100

    [back][auth] Add JavaFaker library for generating fake user data

--- backend/services/auth-service/src/main/java/com/adelium/web/authservice/SetupDataLoader.java
+++ backend/services/auth-service/src/main/java/com/adelium/web/authservice/SetupDataLoader.java
@@ -8,12 +8,14 @@ import com.adelium.web.authservice.entity.User;
 import com.adelium.web.authservice.repository.AuthorityRepository;
 import com.adelium.web.authservice.repository.RoleRepository;
 import com.adelium.web.authservice.repository.UserRepository;
+import com.github.javafaker.Faker;
 import jakarta.transaction.Transactional;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -28,6 +30,7 @@ import org.springframework.stereotype.Component;
  * @see Component
  */
 @Component
+@Profile("dev")
 @RequiredArgsConstructor
 public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -41,6 +44,14 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
 
     private final PasswordEncoder passwordEncoder;
 
+    private final Faker faker = new Faker();
+
+    /**
+     * This method is called when the application starts.
+     * It creates default roles, authorities, and a user with admin privileges if they do not exist in the database.
+     *
+     * @param event The ContextRefreshedEvent event.
+     */
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -62,26 +73,56 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        User user = new User();
-        user.setUsername("admin@adelium.com");
-        user.setFirstname("hakim");
-        user.setLastname("saoudi");
-        user.setPhone("0123456789");
-        user.setPassword(passwordEncoder.encode("password"));
-        user.getRoles().add(adminRole);
-        user.setEnabled(true);
-        user.setAddress(
-                Address.builder()
-                        .streetNumber("1")
-                        .street("Rue de la paix")
-                        .postalCode("01100")
-                        .city("Oyonnax")
-                        .country("France")
-                        .build());
-        userRepository.save(user);
+
+        // Create admin user
+        createUser("admin@adelium.com", "hakim", "saoudi", "0123456789", "password", adminRole);
+
+        // Create 100 users
+        for (int i = 0; i < 100; i++) {
+            createUser(
+                    faker.internet().emailAddress(),
+                    faker.name().firstName(),
+                    faker.name().lastName(),
+                    faker.phoneNumber().phoneNumber(),
+                    "password",
+                    adminRole);
+        }
+
         alreadySetup = true;
     }
 
+    private void createUser(
+            String email,
+            String firstname,
+            String lastname,
+            String phone,
+            String password,
+            Role role) {
+        User user = new User();
+        user.setUsername(email);
+        user.setFirstname(firstname);
+        user.setLastname(lastname);
+        user.setPhone(phone);
+        user.setPassword(passwordEncoder.encode(password));
+        user.getRoles().add(role);
+        user.setEnabled(true);
+        user.setAddress(
+                Address.builder()
+                        .streetNumber(faker.address().streetAddressNumber())
+                        .street(faker.address().streetName())
+                        .postalCode(faker.address().zipCode())
+                        .city(faker.address().city())
+                        .country(faker.address().country())
+                        .build());
+        userRepository.save(user);
+    }
+
+    /**
+     * This method creates an authority if it does not exist in the database.
+     *
+     * @param name The name of the authority.
+     * @return The authority.
+     */
     @Transactional
     public Authority createAuthorityIfNotFound(String name) {
 
@@ -96,6 +137,13 @@ public class SetupDataLoader implements ApplicationListener<ContextRefreshedEven
         return authority;
     }
 
+    /**
+     * This method creates a role if it does not exist in the database.
+     *
+     * @param name        The name of the role.
+     * @param authorities The authorities of the role.
+     * @return The role.
+     */
     @Transactional
     public Role createRoleIfNotFound(String name, Collection<Authority> authorities) {
 
