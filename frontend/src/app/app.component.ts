09e937ad15d21c8e97528f7cda384a7b6ef80a04
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-03 22:42:22 +0100

    front: Refactor app initialization action

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store'
-import { initTheme } from '@store/theme/theme.actions'
+import { appInit } from '@store/app/app.actions'
 
 @Component({
     selector: 'app-root',
@@ -12,6 +12,6 @@ export class AppComponent implements OnInit {
     constructor(private store: Store) {}
 
     ngOnInit(): void {
-        this.store.dispatch(initTheme())
+        this.store.dispatch(appInit())
     }
 }
