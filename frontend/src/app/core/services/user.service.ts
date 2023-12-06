14f0986780a00d37cf05d96673dd7dd9ee2a735b
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-07 00:00:34 +0100

    front: Delete unused service and directive files

--- frontend/src/app/core/services/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core'
-import { User } from '@core/entity/user.entity'
-import { environment } from 'src/environments/environment.development'
-import { RestService } from './rest.service'
-
-@Injectable({ providedIn: 'root' })
-export class UserService extends RestService<User> {
-    override url = `${environment.baseUrl}/api/v1/auth/users`
-}
