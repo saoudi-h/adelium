10e6434b730a91d2d642384d7005e741e87d10c2
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-11 03:41:01 +0100

    back:common: Add ForbiddenHandler and UnauthorizedHandler classes for security handling

---
+++ backend/common/src/main/java/com/adelium/web/common/security/ForbiddenHandler.java
@@ -0,0 +1,22 @@
+/* (C)2023 */
+package com.adelium.web.common.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForbiddenHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException)
+            throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage());
+    }
+}
