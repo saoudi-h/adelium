f6684b88e19bd40bc4f080f72a77ba7a4f8f4dd9
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-12 09:52:45 +0100

    front: Add NotificationService to RefreshInterceptor

--- frontend/src/app/core/interceptors/refresh.interceptor.ts
+++ frontend/src/app/core/interceptors/refresh.interceptor.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { RequestQueueService } from '@auth/services/request-queue.service'
+import { NotificationService } from '@core/services/notification.service'
 import { Store } from '@ngrx/store'
 import { AppState } from '@reducers'
 import * as AuthActions from '@store/auth/auth.actions'
@@ -20,7 +21,8 @@ export class RefreshInterceptor implements HttpInterceptor {
 
     constructor(
         private requestQueueService: RequestQueueService,
-        private store: Store<AppState>
+        private store: Store<AppState>,
+        private notification: NotificationService
     ) {}
 
     intercept(
@@ -29,18 +31,23 @@ export class RefreshInterceptor implements HttpInterceptor {
     ): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(
             catchError((error: HttpErrorResponse) => {
-                if (
-                    error.status === 401 &&
-                    !request.url.endsWith(this.refreshEndpoint)
-                ) {
-                    this.requestQueueService.enqueueRequest(request, next)
-                    this.store.dispatch(AuthActions.refreshToken())
-                    return throwError(
-                        () =>
-                            new Error(
-                                'La requête a été mise en attente en attendant le rafraîchissement du token'
-                            )
-                    )
+                if (error.status === 401) {
+                    if (!request.url.endsWith(this.refreshEndpoint)) {
+                        this.requestQueueService.enqueueRequest(request, next)
+                        this.store.dispatch(AuthActions.refreshToken())
+                        return throwError(
+                            () =>
+                                new Error(
+                                    'La requête a été mise en attente en attendant le rafraîchissement du token'
+                                )
+                        )
+                    } else {
+                        this.store.dispatch(AuthActions.logout())
+                        this.notification.error(
+                            'Session expirée',
+                            'Votre session a expiré, veuillez vous reconnecter'
+                        )
+                    }
                 }
                 return throwError(() => error)
             })
