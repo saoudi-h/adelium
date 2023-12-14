72da1a95ea7f4e1a3c8a28033518469091cadf2e
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-14 23:54:45 +0100

    front: Add user form to AdminUsersComponent

--- frontend/src/app/admin/pages/users/users.component.ts
+++ frontend/src/app/admin/pages/users/users.component.ts
@@ -1,3 +1,13 @@
+import {
+    CheckboxForm,
+    EmailInput,
+    PasswordInput,
+    SelectForm,
+    TelInput,
+    TextInput,
+    UrlInput,
+} from '@admin/forms/Forms'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AdminConfig } from '@admin/components/base-admin/admin-config.types'
 import { BaseAdminComponent } from '@admin/components/base-admin/base-admin.component'
 import { ViewLayoutComponent } from '@admin/components/base-admin/view-layout.component'
@@ -10,11 +20,14 @@ import {
 } from '@angular/animations'
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { Validators } from '@angular/forms'
 import { User } from '@core/entity/user.entity'
+import { CustomValidators } from '@core/utility/CustomValidators'
 import { SharedModule } from '@shared/shared.module'
 import { UserActions } from '@store/user/user.actions'
 import { UserSelectors } from '@store/user/user.selectors'
 import { Observable } from 'rxjs'
+import { EntityFormModel } from './../../forms/forms.types'
 import { UserAdminTrComponent } from './user-admin-tr.component'
 @Component({
     standalone: true,
@@ -114,6 +127,7 @@ import { UserAdminTrComponent } from './user-admin-tr.component'
         view-layout
         [config]="config"
         [paginationResult$]="paginationResult$"
+        (add)="onAdd()"
         (pageChange)="onPageChange($event)">
         <!-- tbody -->
         <!-- <tbody
@@ -178,5 +192,130 @@ export class AdminUsersComponent extends BaseAdminComponent<User> {
             'Actions',
         ],
         subtitle: 'Ajouter, modifier et supprimer des utilisateurs',
+    }
+    override entityFormModel: EntityFormModel<User> = {
+        onFormSubmit: (formValue: any) => {
+            console.log(formValue)
+        },
+        title: 'Utilisateur',
+        id: 'user',
+        actions: [
+            {
+                label: 'Enregistrer',
+                type: 'submit',
+                color: 'primary',
+                icon: 'save-icon',
+                action: () => {
+                    console.log('save')
+                },
+            },
+        ],
+        additionalInfo: 'Veuillez remplir les champs suivants',
+        fields: [
+            {
+                id: 'firstname',
+                type: TextInput,
+                label: 'Prénom',
+                placeholder: 'John',
+                validators: [Validators.required, Validators.maxLength(50)],
+            },
+            {
+                id: 'lastname',
+                type: TextInput,
+                label: 'Nom',
+                placeholder: 'Doe',
+                validators: [Validators.required, Validators.maxLength(50)],
+            },
+            {
+                id: 'username',
+                type: EmailInput,
+                label: 'Username',
+                placeholder: 'my@adelium.io',
+                validators: [
+                    Validators.required,
+                    Validators.email,
+                    Validators.maxLength(50),
+                ],
+            },
+            {
+                id: 'phone',
+                type: TelInput,
+                label: 'Téléphone',
+                placeholder: '0606060606',
+                validators: [Validators.required, Validators.maxLength(50)],
+            },
+            {
+                id: 'password',
+                type: PasswordInput,
+                label: 'Mot de passe',
+                placeholder: '********',
+                validators: [
+                    Validators.required,
+                    Validators.maxLength(50),
+                    Validators.minLength(8),
+                    CustomValidators.password,
+                ],
+            },
+            {
+                id: 'role',
+                type: SelectForm,
+                label: 'Role',
+                placeholder: 'Utilisateur',
+                options: [
+                    {
+                        label: 'Utilisateur',
+                        value: 'user',
+                    },
+                    {
+                        label: 'Administrateur',
+                        value: 'admin',
+                    },
+                ],
+                validators: [Validators.required, Validators.maxLength(50)],
+            },
+            {
+                id: 'accountNonExpired',
+                type: CheckboxForm,
+                label: 'Compte non expiré',
+                validators: [Validators.required],
+            },
+            {
+                id: 'accountNonLocked',
+                type: CheckboxForm,
+                label: 'Compte non verrouillé',
+                validators: [Validators.required],
+            },
+            {
+                id: 'credentialsNonExpired',
+                type: CheckboxForm,
+                label: 'Mot de passe non expiré',
+                validators: [Validators.required],
+            },
+            {
+                id: 'enabled',
+                type: CheckboxForm,
+                label: 'Activé',
+                validators: [Validators.required],
+            },
+            {
+                id: 'isVerified',
+                type: CheckboxForm,
+                label: 'vérifié',
+                validators: [Validators.required],
+            },
+            {
+                id: 'address',
+                type: TextInput,
+                label: 'Addresse',
+                validators: [Validators.required],
+            },
+            {
+                id: 'avatar',
+                type: UrlInput,
+                label: 'Avatar',
+                placeholder: 'https://myavatar.com',
+                validators: [Validators.required],
+            },
+        ],
     }
 }
