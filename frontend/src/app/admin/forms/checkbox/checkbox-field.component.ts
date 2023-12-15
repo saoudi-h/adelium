43759ddbe584c29424b135e49bcca85c7c61e2a0
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-15 22:09:40 +0100

    front: Add checkbox and input field components

---
+++ frontend/src/app/admin/forms/checkbox/checkbox-field.component.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Input } from '@angular/core'
+import { FormFieldConfig } from '../forms.types'
+
+@Component({
+    standalone: true,
+    selector: '[checkbox-field]',
+    template: ` <label class="form-control w-full max-w-xs">
+        <div class="label">
+            <span class="label-text" [attr.for]="field.id">{{
+                field.label
+            }}</span>
+        </div>
+        <input
+            [id]="field.id"
+            type="checkbox"
+            [checked]="
+                initialValue?.[field.id] ? initialValue?.[field.id] : false
+            "
+            class="checkbox-primary checkbox" />
+    </label>`,
+})
+export class CheckBoxFieldComponent {
+    @Input() field!: FormFieldConfig
+    @Input() initialValue?: any
+}
---
+++ frontend/src/app/admin/forms/input/input-field.component.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, Input } from '@angular/core'
+import { FormFieldConfig } from '../forms.types'
+
+@Component({
+    standalone: true,
+    selector: '[input-field]',
+    template: `
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text" [attr.for]="field.id">{{
+                    field.label
+                }}</span>
+            </div>
+            <input
+                class="input input-bordered w-full max-w-xs"
+                [id]="field.id"
+                [type]="field.type.option"
+                [placeholder]="
+                    field.placeholder ? field.placeholder : field.label
+                "
+                [value]="initialValue ? initialValue : ''"
+                class="input input-bordered" />
+        </label>
+    `,
+})
+export class InputFieldComponent {
+    @Input() field!: FormFieldConfig
+    @Input() initialValue?: any
+}
