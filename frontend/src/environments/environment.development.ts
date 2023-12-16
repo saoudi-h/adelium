457001a1adc43efd632382c6cb39c3ab1ab0afee
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-16 19:29:51 +0100

    front: Add role and authority endpoints to development environment

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: false,
     baseUrl: 'http://localhost:8080',
     userEndPoint: '/api/v1/auth/users',
+    roleEndPoint: '/api/v1/auth/roles',
+    authorityEndPoint: '/api/v1/auth/authorities',
     maxRefreshAttempts: 3,
 }
