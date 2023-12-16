5e75b80619cf8cf0ef7a5dc1f79adf70736bde65
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-16 19:31:06 +0100

    front: Add new icon components

---
+++ frontend/src/app/shared/components/icons/add-icon.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+    selector: '[add-icon]',
+    standalone: true,
+    template: `<svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        height="100%"
+        width="100%"
+        class="{{ className }}">
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>`,
+})
+export class AddIconComponent {
+    @Input() className = ''
+}
