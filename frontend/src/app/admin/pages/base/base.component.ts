875c8f7125ecb03a245dc7da884702245a12b466
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-19 23:58:16 +0100

    front: Update BaseAdminComponent in base.component.ts

--- frontend/src/app/admin/pages/base/base.component.ts
+++ frontend/src/app/admin/pages/base/base.component.ts
@@ -1,6 +1,6 @@
 import { EntityFormModel } from '@admin/forms/forms.types'
 import { CommonModule } from '@angular/common'
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Identifiable } from '@core/entity/identifiable.interface'
 import { FormModalService } from '@core/services/formModal.service'
 import { ModalService } from '@core/services/modal.service'
@@ -8,9 +8,12 @@ import { Store } from '@ngrx/store'
 import { AppState } from '@reducers'
 import { SharedModule } from '@shared/shared.module'
 import { EntityActions } from '@store/generic/generic.actions'
-import { PaginationResult } from '@store/generic/generic.reducer'
+import {
+    PaginationParams,
+    PaginationResult,
+} from '@store/generic/generic.reducer'
 import { EntitySelectors } from '@store/generic/generic.selectors'
-import { Observable, catchError, first, map, of } from 'rxjs'
+import { Observable, Subscription, catchError, first, map, of, tap } from 'rxjs'
 import { baseAnimations } from './base-animations.animation'
 import { BaseTrComponent } from './base-tr.component'
 import { AdminConfig } from './components/admin-config.types'
@@ -24,7 +27,11 @@ import { ViewLayoutComponent } from './components/view-layout.component'
     styleUrl: './base.component.sass',
     animations: baseAnimations,
 })
-export class BaseAdminComponent<T extends Identifiable> implements OnInit {
+export class BaseAdminComponent<T extends Identifiable>
+    implements OnInit, OnDestroy
+{
+    private subscription = new Subscription()
+    paginationParams$!: Observable<PaginationParams>
     selectors!: EntitySelectors<T>
     actions!: EntityActions<T>
     entities$!: Observable<T[]>
@@ -39,69 +46,82 @@ export class BaseAdminComponent<T extends Identifiable> implements OnInit {
         protected modalService: ModalService,
         protected formModalService: FormModalService<T>
     ) {}
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe()
+    }
     ngOnInit(): void {
-        this.store.dispatch(
-            this.actions.getPage({ params: { page: 0, size: 10, sort: [] } })
+        this.paginationParams$ = this.store.select(
+            this.selectors.selectPaginationParams
         )
+
+        this.subscription.add(
+            this.paginationParams$
+                .pipe(
+                    tap(params => {
+                        this.store.dispatch(this.actions.getPage({ params }))
+                    })
+                )
+                .subscribe()
+        )
+
         this.paginationResult$ = this.store.select(
             this.selectors.selectPaginationResult
         )
-        this.entities$ = this.store.select(this.selectors.selectAll)
+        this.entities$ = this.store.select(this.selectors.selectCurrentPage)
         this.isLoading$ = this.store.select(this.selectors.selectIsLoading)
         this.error$ = this.store.select(this.selectors.selectError)
     }
 
     onDelete(id: number) {
-        this.entities$ = this.entities$.pipe(
-            map(entities =>
-                entities.map(entity => {
-                    if (entity.id === id) {
-                        return { ...entity, isDeleting: true }
-                    }
-                    return entity
-                })
-            )
-        )
         // Dispatcher l'action de suppression
         this.modalService.openModal({
             type: 'confirmation',
             title: 'Confirmation nécessaire',
             message: 'Êtes-vous sûr de vouloir continuer ?',
             isClosable: true,
-            onConfirm: () =>
-                this.store.dispatch(this.actions.deleteItem({ id })),
+            onConfirm: () => {
+                setTimeout(() => {
+                    this.store.dispatch(this.actions.deleteItem({ id })), 500
+                })
+                this.entities$ = this.entities$.pipe(
+                    map(entities =>
+                        entities.map(entity => {
+                            if (entity.id === id) {
+                                return { ...entity, isDeleting: true }
+                            }
+                            return entity
+                        })
+                    )
+                )
+            },
         })
     }
 
     onEdit(id: number) {
-        this.store
-            .select(this.selectors.selectEntityById(id))
-            .pipe(
-                first(),
-                catchError(error => {
-                    console.log(
-                        "Erreur lors de la récupération de l'entité",
-                        error
-                    )
-                    return of(undefined)
+        this.subscription.add(
+            this.store
+                .select(this.selectors.selectEntityById(id))
+                .pipe(
+                    first(),
+                    catchError(error => {
+                        console.log(
+                            "Erreur lors de la récupération de l'entité",
+                            error
+                        )
+                        return of(undefined)
+                    })
+                )
+                .subscribe(entity => {
+                    this.formModalService.openFormModal({
+                        ...this.entityFormModel,
+                        initialValue: entity,
+                    })
                 })
-            )
-            .subscribe(entity => {
-                this.formModalService.openFormModal({
-                    ...this.entityFormModel,
-                    initialValue: entity,
-                })
-            })
+        )
     }
 
     onPageChange(page: number) {
-        console.log('onPageChange base-admin', page)
-        console.log(page)
-        this.store.dispatch(
-            this.actions.getPage({
-                params: { page: page, size: 10, sort: [] },
-            })
-        )
+        this.store.dispatch(this.actions.updatePaginationParams({ page }))
     }
 
     onAdd() {
