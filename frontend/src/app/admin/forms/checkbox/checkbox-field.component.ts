cc3da69f454c38375a5e5b5c375b736fa9d64788
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-21 23:51:11 +0100

    front: Refactor form field components

--- frontend/src/app/admin/forms/checkbox/checkbox-field.component.ts
+++ frontend/src/app/admin/forms/checkbox/checkbox-field.component.ts
@@ -1,26 +1,31 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, Input } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { FormField } from '../forms.types'
 
 @Component({
     standalone: true,
+    imports: [ReactiveFormsModule],
     selector: '[checkbox-field]',
-    template: ` <label class="form-control w-full max-w-xs">
+    template: ` <label
+        class="form-control mb-4 w-full max-w-xs flex-row items-center gap-3"
+        [formGroup]="group">
+        <input
+            [id]="field.id"
+            type="checkbox"
+            class="checkbox checkbox-xs"
+            [formControlName]="field.id" />
         <div class="label">
             <span class="label-text" [attr.for]="field.id">{{
                 field.label
             }}</span>
         </div>
-        <input
-            [id]="field.id"
-            type="checkbox"
-            [checked]="
-                initialValue?.[field.id] ? initialValue?.[field.id] : false
-            "
-            class="checkbox-primary checkbox" />
     </label>`,
 })
 export class CheckBoxFieldComponent {
+    getControl(fildName: string): FormControl {
+        return this.group.get(fildName) as FormControl
+    }
+    @Input() group!: FormGroup
     @Input() field!: FormField
-    @Input() initialValue?: any
 }
--- frontend/src/app/admin/forms/input/input-field.component.ts
+++ frontend/src/app/admin/forms/input/input-field.component.ts
@@ -1,30 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, Input } from '@angular/core'
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms'
+import { FieldStatusComponent } from '@shared/components/form/field-status.component'
 import { FormField } from '../forms.types'
 
 @Component({
     standalone: true,
     selector: '[input-field]',
+    imports: [ReactiveFormsModule, FieldStatusComponent],
     template: `
-        <label class="form-control w-full max-w-xs">
-            <div class="label">
-                <span class="label-text" [attr.for]="field.id">{{
-                    field.label
-                }}</span>
+        <div field-status [control]="getControl(field.id)">
+            <div class="group relative z-0 w-full" [formGroup]="group">
+                <input
+                    [id]="field.id"
+                    [type]="'text'"
+                    placeholder=""
+                    [name]="field.id"
+                    class="peer block w-full appearance-none border-0 border-b-2 border-base-200 bg-transparent px-0 py-2.5 text-sm text-base-content/70 focus:border-accent focus:outline-none focus:ring-0"
+                    [formControlName]="field.id"
+                    [required]="isRequired()" />
+                <label
+                    [for]="field.id"
+                    class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-base-content/50 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-accent"
+                    >{{ field.label }}</label
+                >
             </div>
-            <input
-                class="input input-bordered w-full max-w-xs"
-                [id]="field.id"
-                [type]="field.type.option"
-                [placeholder]="
-                    field.placeholder ? field.placeholder : field.label
-                "
-                [value]="initialValue ? initialValue : ''"
-                class="input input-bordered" />
-        </label>
+        </div>
     `,
 })
 export class InputFieldComponent {
+    getControl(fildName: string): FormControl {
+        return this.group.get(fildName) as FormControl
+    }
+    @Input() group!: FormGroup
     @Input() field!: FormField
-    @Input() initialValue?: any
+
+    isRequired(): boolean {
+        return this.field.validators?.includes(Validators.required) ?? false
+    }
 }
