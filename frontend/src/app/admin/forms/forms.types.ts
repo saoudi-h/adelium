900c9e5c317bd288cd7d7f186acdcac3d669f0ab
Hakim Saoudi <saoudihakim@gmail.com>
2023-12-27 23:00:55 +0100

    front: Add ActionType and actionType property to EntityFormModel

--- frontend/src/app/admin/forms/forms.types.ts
+++ frontend/src/app/admin/forms/forms.types.ts
@@ -5,15 +5,19 @@ import { Identifiable } from '@core/entity/identifiable.interface'
 import { PaginationParams } from '@store/generic/generic.reducer'
 import { Observable } from 'rxjs'
 
+export type ActionType = 'add' | 'edit' | 'delete' | 'preview' | 'patch'
+
 export interface EntityFormModel<T extends Identifiable> {
     title: string
     fields: FormField[]
     id?: number | string
-    actions?: FormAction[]
     additionalInfo?: string
     initialValue?: T
     formValidators?: ValidatorFn[]
-    onFormSubmit: (formValue: any) => void
+    actions?: FormAction[]
+    actionType: ActionType
+    onEdit: (form: T) => void
+    onAdd: (form: T) => void
 }
 
 export type FieldType = {
@@ -75,7 +79,11 @@ export interface FormField {
             id: number
         ) => Observable<{ label: string; value: any; disabled?: boolean }[]>
         paginationResult: () => Observable<PaginationResult>
-        reverseSelection?: (value: any) => any
+        setRelations?: (
+            id: number,
+            relation: string,
+            relatedEntityIds: number[]
+        ) => void
     }
     addTag?: boolean
     fields?: FormField[]
