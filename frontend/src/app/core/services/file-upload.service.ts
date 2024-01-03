06a9686c0efa45ac8543ded6ed5f50ee5a8b3388
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-03 06:31:43 +0100

    front: Add FileUploadService for handling file uploads

---
+++ frontend/src/app/core/services/file-upload.service.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { HttpClient, HttpEvent, HttpEventType } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment.development'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class FileUploadService {
+    private url = `${environment.baseUrl}${environment.mediaUrl}`
+
+    constructor(private http: HttpClient) {}
+
+    uploadFile(file: File): Observable<string> {
+        const formData: FormData = new FormData()
+        formData.append('file', file, file.name)
+
+        const uploadUrl = `${this.url}/files/upload/true`
+
+        return new Observable(observer => {
+            this.http
+                .post<{ url: string }>(uploadUrl, formData, {
+                    reportProgress: true,
+                    observe: 'events',
+                })
+                .subscribe({
+                    next: (event: HttpEvent<any>) => {
+                        switch (event.type) {
+                            case HttpEventType.Response:
+                                if (event.body && event.body.url) {
+                                    observer.next(event.body.url)
+                                    observer.complete()
+                                } else {
+                                    observer.error(
+                                        'URL non retournée par le serveur'
+                                    )
+                                }
+                                break
+                        }
+                    },
+                    error: error => {
+                        observer.error(error)
+                    },
+                })
+        })
+    }
+}
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,16 +1,21 @@
 export const environment = {
     production: false,
     baseUrl: 'http://localhost:8080',
+
+    // Auth-service
     userEndPoint: '/api/v1/auth/users',
     roleEndPoint: '/api/v1/auth/roles',
     authorityEndPoint: '/api/v1/auth/authorities',
     maxRefreshAttempts: 3,
 
-    // Quiz
+    // Quiz-service
     quizDefaultUrl: '/api/v1/quiz/quiz-defaults',
     quizMcqUrl: '/api/v1/quiz/quiz-mcqs',
     // Bank
     bankDefaultUrl: '/api/v1/quiz/bank-defaults',
     // Tag
     tagUrl: '/api/v1/quiz/tags',
+
+    // Media-service
+    mediaUrl: '/api/v1/media',
 }
