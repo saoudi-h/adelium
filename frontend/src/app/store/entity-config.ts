ad5607dc4fb9399be069a116c9c1b2cec2da42c0
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-03 13:36:17 +0100

    front: Add entity actions and relations for bank defaults, quiz defaults, quiz MCQ, and tags

--- frontend/src/app/store/entity-config.ts
+++ frontend/src/app/store/entity-config.ts
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { RoleActions } from './entities/auth/role/role.actions'
 import { UserActions } from './entities/auth/user/user.actions'
+import { BankDefaultActions } from './entities/evaluation/bank/bank-default/bank-default.actions'
+import { QuizDefaultActions } from './entities/evaluation/quiz/quiz-default/quiz-default.actions'
+import { QuizMcqActions } from './entities/evaluation/quiz/quiz-mcq/quiz-mcq.actions'
+import { TagActions } from './entities/evaluation/tag/tag.actions'
 
 export const entityConfig: { [key: string]: { actions: any; relations: any } } =
     {
@@ -15,5 +19,27 @@ export const entityConfig: { [key: string]: { actions: any; relations: any } } =
             relations: {
                 roles: { type: 'manyToMany', partialUrl: 'users' },
             },
+        },
+        bankDefaults: {
+            actions: BankDefaultActions,
+            relations: {
+                tags: { type: 'manyToMany', partialUrl: 'tags' },
+            },
+        },
+        quizDefaults: {
+            actions: QuizDefaultActions,
+            relations: {
+                tags: { type: 'manyToMany', partialUrl: 'tags' },
+            },
+        },
+        quizMcq: {
+            actions: QuizMcqActions,
+            relations: {
+                tags: { type: 'manyToMany', partialUrl: 'tags' },
+            },
+        },
+        tags: {
+            actions: TagActions,
+            relations: {},
         },
     }
