aab36282c5a635b18fd4d744994144b6b0ccbf52
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-04 08:27:05 +0100

    front: Add icons to page-size component

--- frontend/src/app/shared/components/utility/page-size/page-size-component.ts
+++ frontend/src/app/shared/components/utility/page-size/page-size-component.ts
@@ -1,23 +1,25 @@
 import { CommonModule } from '@angular/common'
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { AddIconComponent } from '@shared/components/icons/add-icon.component'
+import { MinusIconComponent } from '@shared/components/icons/minus-icon.component'
 
 @Component({
     selector: '[page-size]',
     standalone: true,
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, MinusIconComponent, AddIconComponent],
     template: `
         <div class="join">
             <button class="btn btn-outline join-item" (click)="decrement()">
-                «
+                <div minus-icon className="h-5 w-5"></div>
             </button>
             <input
                 type="number"
                 [(ngModel)]="size"
                 (change)="onChange()"
-                class="input join-item input-bordered max-w-[5rem] border-base-content bg-base-200" />
+                class="input join-item input-bordered max-w-[5rem] border-base-content bg-base-200 text-center font-semibold" />
             <button class="btn btn-outline join-item" (click)="increment()">
-                »
+                <div add-icon className="h-5 w-5"></div>
             </button>
         </div>
     `,
