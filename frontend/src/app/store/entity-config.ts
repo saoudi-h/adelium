ce5a0ed984b2d4b07e3e939b1da23e1a946516c3
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-09 20:59:30 +0100

    front: Add environment URIs to entityConfig

--- frontend/src/app/store/entity-config.ts
+++ frontend/src/app/store/entity-config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { environment } from 'src/environments/environment.development'
 import { RoleActions } from './entities/auth/role/role.actions'
 import { UserActions } from './entities/auth/user/user.actions'
 import { BankDefaultActions } from './entities/evaluation/bank/bank-default/bank-default.actions'
@@ -10,69 +11,80 @@ import { QuizDefaultActions } from './entities/evaluation/quiz/quiz-default/quiz
 import { QuizMcqActions } from './entities/evaluation/quiz/quiz-mcq/quiz-mcq.actions'
 import { TagActions } from './entities/evaluation/tag/tag.actions'
 
-export const entityConfig: { [key: string]: { actions: any; relations: any } } =
-    {
-        users: {
-            actions: UserActions,
-            relations: {
-                roles: { type: 'manyToMany', partialUrl: 'roles' },
+export const entityConfig: {
+    [key: string]: { actions: any; relations: any; uri?: string }
+} = {
+    users: {
+        actions: UserActions,
+        relations: {
+            roles: { type: 'manyToMany', partialUrl: 'roles' },
+        },
+        uri: environment.userEndPoint,
+    },
+    roles: {
+        actions: RoleActions,
+        relations: {
+            roles: { type: 'manyToMany', partialUrl: 'users' },
+        },
+        uri: environment.roleEndPoint,
+    },
+    bankDefaults: {
+        actions: BankDefaultActions,
+        relations: {
+            tags: { type: 'manyToMany', partialUrl: 'tags' },
+        },
+        uri: environment.bankDefaultUrl,
+    },
+    quizDefaults: {
+        actions: QuizDefaultActions,
+        relations: {
+            tags: { type: 'manyToMany', partialUrl: 'tags' },
+        },
+        uri: environment.quizDefaultUrl,
+    },
+    quizMcq: {
+        actions: QuizMcqActions,
+        relations: {
+            tags: { type: 'manyToMany', partialUrl: 'tags' },
+        },
+        uri: environment.quizMcqUrl,
+    },
+    tags: {
+        actions: TagActions,
+        relations: {},
+        uri: environment.tagUrl,
+    },
+    questionMcqs: {
+        actions: QuestionMcqActions,
+        relations: {
+            tags: { type: 'manyToMany', partialUrl: 'tags' },
+            options: { type: 'oneToMany', partialUrl: 'options' },
+        },
+        uri: environment.questionMcqUrl,
+    },
+    questionTrueFalses: {
+        actions: QuestionTrueFalseActions,
+        relations: {
+            tags: { type: 'manyToMany', partialUrl: 'tags' },
+            options: { type: 'oneToMany', partialUrl: 'options' },
+        },
+        uri: environment.questionTrueFalseUrl,
+    },
+    optionMcqs: {
+        actions: OptionMcqActions,
+        relations: {
+            questionMcq: { type: 'manyToOne', partialUrl: 'question-mcqs' },
+        },
+        uri: environment.optionMcqUrl,
+    },
+    optionTrueFalses: {
+        actions: OptionTrueFalseActions,
+        relations: {
+            questionTrueFalse: {
+                type: 'manyToOne',
+                partialUrl: 'question-true-falses',
             },
         },
-        roles: {
-            actions: RoleActions,
-            relations: {
-                roles: { type: 'manyToMany', partialUrl: 'users' },
-            },
-        },
-        bankDefaults: {
-            actions: BankDefaultActions,
-            relations: {
-                tags: { type: 'manyToMany', partialUrl: 'tags' },
-            },
-        },
-        quizDefaults: {
-            actions: QuizDefaultActions,
-            relations: {
-                tags: { type: 'manyToMany', partialUrl: 'tags' },
-            },
-        },
-        quizMcq: {
-            actions: QuizMcqActions,
-            relations: {
-                tags: { type: 'manyToMany', partialUrl: 'tags' },
-            },
-        },
-        tags: {
-            actions: TagActions,
-            relations: {},
-        },
-        questionMcq: {
-            actions: QuestionMcqActions,
-            relations: {
-                tags: { type: 'manyToMany', partialUrl: 'tags' },
-                options: { type: 'oneToMany', partialUrl: 'options' },
-            },
-        },
-        questionTrueFalse: {
-            actions: QuestionTrueFalseActions,
-            relations: {
-                tags: { type: 'manyToMany', partialUrl: 'tags' },
-                options: { type: 'oneToMany', partialUrl: 'options' },
-            },
-        },
-        optionsMcq: {
-            actions: OptionMcqActions,
-            relations: {
-                questionMcq: { type: 'manyToOne', partialUrl: 'question-mcqs' },
-            },
-        },
-        optionsTrueFalse: {
-            actions: OptionTrueFalseActions,
-            relations: {
-                questionTrueFalse: {
-                    type: 'manyToOne',
-                    partialUrl: 'question-true-falses',
-                },
-            },
-        },
-    }
+        uri: environment.optionTrueFalseUrl,
+    },
+}
