178193f03179bdc4ec22985288677042ffdba272
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-09 21:08:55 +0100

    front: Fix typo in QuestionTrueFalseActions

--- frontend/src/app/store/entities/evaluation/question/question-true-false/question-true-false.actions.ts
+++ frontend/src/app/store/entities/evaluation/question/question-true-false/question-true-false.actions.ts
@@ -1,5 +1,6 @@
 import { QuestionTrueFalse } from '@core/entity/evaluation/question-true-false.entity'
 import { createEntityActions } from '@store/generic/generic.actions'
 
-export const QuestionTrueFalseActions =
-    createEntityActions<QuestionTrueFalse>('Question-mcq')
+export const QuestionTrueFalseActions = createEntityActions<QuestionTrueFalse>(
+    'Question-true-false'
+)
