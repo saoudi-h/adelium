c2b8545e06aacbeff1cf0030eb6b482929111f70
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-14 20:11:03 +0100

    back:auth: move getGravatar from User to UserService

--- backend/services/auth-service/src/main/java/com/adelium/web/authservice/service/UserService.java
+++ backend/services/auth-service/src/main/java/com/adelium/web/authservice/service/UserService.java
@@ -4,6 +4,10 @@ package com.adelium.web.authservice.service;
 import com.adelium.web.authservice.mapper.UserDetailsMapper;
 import com.adelium.web.authservice.repository.UserRepository;
 import com.adelium.web.common.dto.UserDetailsDTO;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,5 +31,46 @@ public class UserService implements UserDetailsService {
                                                 "User with username: "
                                                         + username
                                                         + " not found.")));
+    }
+
+    /**
+     * Return the avatar of the user.
+     * from Gravatar if exists, from ui-avatars.com otherwise
+     *
+     * @see <a href="https://fr.gravatar.com/">Gravatar</a>
+     * @see <a href="https://ui-avatars.com/">ui-avatars.com</a>
+     *
+     * @param email the email of the user
+     * @param firstname the firstname of the user
+     * @param lastname the lastname of the user
+     * @return the avatar of the user, null if an error occurs
+     */
+    public String getGravatar(String email, String firstname, String lastname) {
+        try {
+            String trimmedEmail = email.trim().toLowerCase();
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            md.update(trimmedEmail.getBytes());
+
+            byte[] bytes = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+            String encodedName =
+                    URLEncoder.encode(firstname + ' ' + lastname, StandardCharsets.UTF_8);
+
+            return String.format(
+                    "https://www.gravatar.com/avatar/%s?d=%s",
+                    sb.toString(),
+                    URLEncoder.encode(
+                            "https://ui-avatars.com/api/" + encodedName + "/" + 128,
+                            StandardCharsets.UTF_8));
+
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
     }
 }
