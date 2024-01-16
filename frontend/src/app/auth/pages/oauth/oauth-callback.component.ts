e1bf1869d4d14b8c776c5f40cfe71c1ddf269569
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-16 23:34:00 +0100

    front: Add OAuth callback component

--- frontend/src/app/auth/pages/oauth/oauth-callback.component.ts
+++ frontend/src/app/auth/pages/oauth/oauth-callback.component.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Router } from '@angular/router'
 import { NotificationService } from '@core/services/notification.service'
 import { Store } from '@ngrx/store'
 import { providersMap } from '@store/auth/oauth2/providers-map'
+import { Observable } from 'rxjs'
+import { SvgOauthComponent } from './svg-oauth.component'
 
 @Component({
     selector: '[oauth-callback]',
-    template: `<div>oauth page</div>`,
+    templateUrl: './oauth-callback.component.html',
+    imports: [CommonModule, SvgOauthComponent],
     standalone: true,
 })
 export class OauthCallbackComponent implements OnInit {
+    provider: 'google' | 'github' = 'google'
+    isLoading: boolean = true
+    error$: Observable<any> | undefined
     constructor(
         private route: ActivatedRoute,
         private store: Store,
@@ -19,15 +27,16 @@ export class OauthCallbackComponent implements OnInit {
 
     ngOnInit(): void {
         this.route.params.subscribe(params => {
-            const provider: string = params['provider']
-            const actions =
-                providersMap[provider as keyof typeof providersMap]?.actions
+            this.provider = params['provider']
+            const provider =
+                providersMap[this.provider as keyof typeof providersMap]
 
-            if (!actions) {
+            if (!provider) {
                 this.notificationService.error(
                     'Erreur de connexion',
                     "Une erreur est survenue lors de l'authentification. Veuillez réessayer plus tard."
                 )
+                this.isLoading = false
                 this.goToLoginPage()
                 return
             }
@@ -35,16 +44,19 @@ export class OauthCallbackComponent implements OnInit {
             this.route.queryParams.subscribe(queryParams => {
                 const code = queryParams['code']
                 if (code) {
-                    console.log('Provider:', provider)
-                    console.log('OAuth2 Code:', code)
-                    this.store.dispatch(actions.loginRedirectSuccess({ code }))
+                    this.store.dispatch(
+                        provider.actions.loginRedirectSuccess({ code })
+                    )
+                    this.error$ = this.store.select(
+                        provider.selectors.selectAuthError
+                    )
                 } else if (queryParams['error']) {
                     this.notificationService.error(
                         'Erreur de connexion',
                         "Une erreur est survenue lors de l'authentification. Veuillez réessayer plus tard."
                     )
                     this.store.dispatch(
-                        actions.loginRedirectFailure({
+                        provider.actions.loginRedirectFailure({
                             error: {
                                 error: queryParams['error'],
                                 errorDescription:
@@ -53,6 +65,9 @@ export class OauthCallbackComponent implements OnInit {
                             },
                         })
                     )
+                    this.goToLoginPage()
+                } else {
+                    this.goToLoginPage()
                 }
             })
         })
