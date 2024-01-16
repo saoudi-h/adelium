051a2890e0c7c95daba2f44aae364af720ad4ce1
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-16 23:36:38 +0100

    front: Add GitHub and Google OAuth2 client IDs and redirect URLs

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -12,10 +12,13 @@ export const environment = {
     // Oauth2
 
     // Github
+    githubClientId: 'c04e061736a77e0dafc1',
     githubRedirectUrl: 'http://127.0.0.1:4200/auth/login/oauth2/code/github',
     githubLoginUrl: 'https://github.com/login/oauth/authorize',
 
     // Google
+    googleClientId:
+        '769174024079-etii8opes9fikau39j14iqv7c66f0td4.apps.googleusercontent.com',
     googleRedirectUrl: 'http://127.0.0.1:4200/login/oauth2/code/google',
     googleLoginUrl: '/oauth2/authorization/google',
 
