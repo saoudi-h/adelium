15c38d7331b58ca1ac38125e2f7988375a26dd11
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-24 03:32:28 +0100

    back:config: Add Spring Security configuration and credentials

---
+++ backend/services/config-service/src/main/java/com/adelium/web/configservice/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+/* (C)2024 */
+package com.adelium.web.configservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(
+                        csrf ->
+                                csrf.ignoringRequestMatchers(
+                                        "/actuator/**", "/encrypt/**", "/decrypt/**"))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
