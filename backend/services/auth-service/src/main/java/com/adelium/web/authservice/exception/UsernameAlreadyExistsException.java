bbc056b86cf38e68e3034c0c9dc059f9824ff1d2
Hakim Saoudi <saoudihakim@gmail.com>
2024-01-29 20:48:41 +0100

    back:auth: Add javadoc for UsernameAlreadyExistsException

--- backend/services/auth-service/src/main/java/com/adelium/web/authservice/exception/UsernameAlreadyExistsException.java
+++ backend/services/auth-service/src/main/java/com/adelium/web/authservice/exception/UsernameAlreadyExistsException.java
@@ -1,6 +1,9 @@
 /* (C)2023 */
 package com.adelium.web.authservice.exception;
 
+/**
+ * Exception thrown when a username already exists.
+ */
 public class UsernameAlreadyExistsException extends RuntimeException {
 
     public UsernameAlreadyExistsException(String message) {
